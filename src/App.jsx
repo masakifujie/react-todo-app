d84f534a568170bd78c249dc19a4c922b0123670
masakifujie <yan.yan.yan@icloud.com>
2021-04-14 22:22:20 +0900

    未完了セクションをコンポーネントでまとめる

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./styles.css";
-import { InputTodo } from './component/InputTodo';
+import { InputTodo } from "./component/InputTodo";
+import { IncompleteTodos } from "./component/IncompleteTodos";
 
 export const App = () => {
-  const [todoText, setTodoText] = useState(['']);
+  const [todoText, setTodoText] = useState([""]);
   const [incompleteTodos, setIncompleteTodos] = useState([""]);
   const [completeTodos, setCompleteTodos] = useState([""]);
 
@@ -29,7 +30,6 @@ export const App = () => {
     const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
-
   };
 
   const onClickBack = (index) => {
@@ -42,21 +42,16 @@ export const App = () => {
   };
   return (
     <>
-      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd}/>
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
