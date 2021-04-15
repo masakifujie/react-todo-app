e4ea82fdac6b67c010991bccdcf72fa313fd94df
masakifujie <yan.yan.yan@icloud.com>
2021-04-15 19:45:18 +0900

    disabled機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -47,17 +47,20 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodo5個までだよ〜。消化しろ〜</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
         onClickDelete={onClickDelete}
+        
       />
-      
-      <CompleteTodos
-        todos={completeTodos}
-        onClickBack={onClickBack}
-      />
+
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
     </>
   );
 };
